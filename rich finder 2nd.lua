3a564856e25f5dea23023cabe298d232bf8fd62a
clo <98161397+betonblyat@users.noreply.github.com>
2022-08-26 09:53:11 +0300

    Add files via upload

---
+++ rich finder 2nd.lua
@@ -0,0 +1,230 @@
+-- Luazifier v0.1
+-- Settings: Mode > Table | Custom Name > false | Security > Off
+
+-- Setup:
+Luazifier = {} 
+setmetatable(Luazifier, {__metatable = "pastenamehere"})
+
+-- Instances:
+Luazifier.ScreenGui1 = Instance.new("ScreenGui", game:GetService("CoreGui"))
+Luazifier.Frame1 = Instance.new("Frame", Luazifier.ScreenGui1)
+Luazifier.ImageLabel1 = Instance.new("ImageLabel", Luazifier.Frame1)
+Luazifier.Frame2 = Instance.new("Frame", Luazifier.Frame1)
+Luazifier.TextLabel1 = Instance.new("TextLabel", Luazifier.Frame2)
+Luazifier.Frame3 = Instance.new("Frame", Luazifier.Frame1)
+Luazifier.TextLabel2 = Instance.new("TextLabel", Luazifier.Frame3)
+
+-- Properties:
+Luazifier.ScreenGui1.DisplayOrder = 0
+Luazifier.ScreenGui1.Enabled = true
+Luazifier.ScreenGui1.IgnoreGuiInset = false
+Luazifier.ScreenGui1.Name = "test1"
+Luazifier.ScreenGui1.ResetOnSpawn = true
+Luazifier.ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Luazifier.ScreenGui1.Archivable = true
+Luazifier.ScreenGui1.AutoLocalize = true
+
+
+Luazifier.Frame1.Active = false
+Luazifier.Frame1.AnchorPoint = Vector2.new(0.5, 0.5)
+Luazifier.Frame1.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.Frame1.BackgroundTransparency = 1
+Luazifier.Frame1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.Frame1.BorderSizePixel = 1
+Luazifier.Frame1.LayoutOrder = 0
+Luazifier.Frame1.Name = "test2"
+Luazifier.Frame1.Position = UDim2.new(0.5, 0, 0.5, 0)
+Luazifier.Frame1.Rotation = 0
+Luazifier.Frame1.Selectable = false
+Luazifier.Frame1.Size = UDim2.new(0, 546, 0, 100)
+Luazifier.Frame1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.Frame1.Style = Enum.FrameStyle.Custom
+Luazifier.Frame1.Visible = true
+Luazifier.Frame1.ZIndex = 1
+Luazifier.Frame1.Archivable = true
+Luazifier.Frame1.ClipsDescendants = false
+Luazifier.Frame1.AutoLocalize = true
+
+
+Luazifier.ImageLabel1.Active = false
+Luazifier.ImageLabel1.AnchorPoint = Vector2.new(0, 0)
+Luazifier.ImageLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.ImageLabel1.BackgroundTransparency = 1
+Luazifier.ImageLabel1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.ImageLabel1.BorderSizePixel = 0
+Luazifier.ImageLabel1.LayoutOrder = 0
+Luazifier.ImageLabel1.Name = "test3"
+Luazifier.ImageLabel1.Position = UDim2.new(0.41844692826271057, 0, 0.04678363725543022, 0)
+Luazifier.ImageLabel1.Rotation = 0
+Luazifier.ImageLabel1.Selectable = false
+Luazifier.ImageLabel1.Size = UDim2.new(0, 90, 0, 90)
+Luazifier.ImageLabel1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.ImageLabel1.Visible = true
+Luazifier.ImageLabel1.ZIndex = 1
+Luazifier.ImageLabel1.Archivable = true
+Luazifier.ImageLabel1.ClipsDescendants = false
+Luazifier.ImageLabel1.AutoLocalize = true
+Luazifier.ImageLabel1.Image = "http://www.roblox.com/asset/?id=1403367287"
+Luazifier.ImageLabel1.ImageColor3 = Color3.new(1, 1, 1)
+Luazifier.ImageLabel1.ImageRectOffset = Vector2.new(0, 0)
+Luazifier.ImageLabel1.ImageRectSize = Vector2.new(0, 0)
+Luazifier.ImageLabel1.ImageTransparency = 1
+Luazifier.ImageLabel1.ScaleType = Enum.ScaleType.Stretch
+Luazifier.ImageLabel1.SliceScale = 1
+
+
+Luazifier.Frame2.Active = false
+Luazifier.Frame2.AnchorPoint = Vector2.new(0, 0)
+Luazifier.Frame2.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.Frame2.BackgroundTransparency = 1
+Luazifier.Frame2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.Frame2.BorderSizePixel = 1
+Luazifier.Frame2.LayoutOrder = 0
+Luazifier.Frame2.Name = "test4"
+Luazifier.Frame2.Position = UDim2.new(0, 0, 0, 0)
+Luazifier.Frame2.Rotation = 0
+Luazifier.Frame2.Selectable = false
+Luazifier.Frame2.Size = UDim2.new(0, 227, 0, 100)
+Luazifier.Frame2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.Frame2.Style = Enum.FrameStyle.Custom
+Luazifier.Frame2.Visible = true
+Luazifier.Frame2.ZIndex = 1
+Luazifier.Frame2.Archivable = true
+Luazifier.Frame2.ClipsDescendants = true
+Luazifier.Frame2.AutoLocalize = true
+
+
+Luazifier.TextLabel1.Active = false
+Luazifier.TextLabel1.AnchorPoint = Vector2.new(0, 0)
+Luazifier.TextLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel1.BackgroundTransparency = 1
+Luazifier.TextLabel1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.TextLabel1.BorderSizePixel = 1
+Luazifier.TextLabel1.LayoutOrder = 0
+Luazifier.TextLabel1.Name = "test5"
+Luazifier.TextLabel1.Position = UDim2.new(0.996688961982727, 0, 0.25017544627189636, 0)
+Luazifier.TextLabel1.Rotation = 0
+Luazifier.TextLabel1.Selectable = false
+Luazifier.TextLabel1.Size = UDim2.new(0, 228, 0, 50)
+Luazifier.TextLabel1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.TextLabel1.Visible = true
+Luazifier.TextLabel1.ZIndex = 1
+Luazifier.TextLabel1.Archivable = true
+Luazifier.TextLabel1.ClipsDescendants = false
+Luazifier.TextLabel1.AutoLocalize = true
+Luazifier.TextLabel1.Font = Enum.Font.DenkOne
+Luazifier.TextLabel1.Text = "test6"
+Luazifier.TextLabel1.TextColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel1.TextScaled = false
+Luazifier.TextLabel1.TextSize = 35
+Luazifier.TextLabel1.TextStrokeColor3 = Color3.new(0, 0, 0)
+Luazifier.TextLabel1.TextStrokeTransparency = 1
+Luazifier.TextLabel1.TextTransparency = 0
+Luazifier.TextLabel1.TextTruncate = Enum.TextTruncate.None
+Luazifier.TextLabel1.TextWrapped = false
+Luazifier.TextLabel1.TextXAlignment = Enum.TextXAlignment.Center
+Luazifier.TextLabel1.TextYAlignment = Enum.TextYAlignment.Center
+
+
+Luazifier.Frame3.Active = false
+Luazifier.Frame3.AnchorPoint = Vector2.new(0, 0)
+Luazifier.Frame3.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.Frame3.BackgroundTransparency = 1
+Luazifier.Frame3.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.Frame3.BorderSizePixel = 1
+Luazifier.Frame3.LayoutOrder = 0
+Luazifier.Frame3.Name = "test7"
+Luazifier.Frame3.Position = UDim2.new(0.5824176073074341, 0, 0, 0)
+Luazifier.Frame3.Rotation = 0
+Luazifier.Frame3.Selectable = false
+Luazifier.Frame3.Size = UDim2.new(0, 228, 0, 100)
+Luazifier.Frame3.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.Frame3.Style = Enum.FrameStyle.Custom
+Luazifier.Frame3.Visible = true
+Luazifier.Frame3.ZIndex = 1
+Luazifier.Frame3.Archivable = true
+Luazifier.Frame3.ClipsDescendants = true
+Luazifier.Frame3.AutoLocalize = true
+
+
+Luazifier.TextLabel2.Active = false
+Luazifier.TextLabel2.AnchorPoint = Vector2.new(0, 0)
+Luazifier.TextLabel2.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel2.BackgroundTransparency = 1
+Luazifier.TextLabel2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.TextLabel2.BorderSizePixel = 1
+Luazifier.TextLabel2.LayoutOrder = 0
+Luazifier.TextLabel2.Name = "test8"
+Luazifier.TextLabel2.Position = UDim2.new(-1.0022845268249512, 0, 0.25017544627189636, 0)
+Luazifier.TextLabel2.Rotation = 0
+Luazifier.TextLabel2.Selectable = false
+Luazifier.TextLabel2.Size = UDim2.new(0, 228, 0, 50)
+Luazifier.TextLabel2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.TextLabel2.Visible = true
+Luazifier.TextLabel2.ZIndex = 1
+Luazifier.TextLabel2.Archivable = true
+Luazifier.TextLabel2.ClipsDescendants = false
+Luazifier.TextLabel2.AutoLocalize = true
+Luazifier.TextLabel2.Font = Enum.Font.DenkOne
+Luazifier.TextLabel2.Text = "test9"
+Luazifier.TextLabel2.TextColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel2.TextScaled = false
+Luazifier.TextLabel2.TextSize = 35
+Luazifier.TextLabel2.TextStrokeColor3 = Color3.new(0, 0, 0)
+Luazifier.TextLabel2.TextStrokeTransparency = 1
+Luazifier.TextLabel2.TextTransparency = 0
+Luazifier.TextLabel2.TextTruncate = Enum.TextTruncate.None
+Luazifier.TextLabel2.TextWrapped = false
+Luazifier.TextLabel2.TextXAlignment = Enum.TextXAlignment.Center
+Luazifier.TextLabel2.TextYAlignment = Enum.TextYAlignment.Center
+
+
+-- Scripts:
+
+local getByName = function(N)
+    for i,v in pairs(Luazifier) do
+        if v.Name and v.Name == N then
+            return v
+        end
+    end
+    return nil
+end
+
+local Flag = getByName("test10")
+local LeftSideText = getByName("hi")
+local RightSideText = getByName("buddy")
+
+while task.wait() do
+    if Flag.ImageTransparency == 0 then
+        break
+    end
+    Flag.ImageTransparency = Flag.ImageTransparency - 0.01
+end
+
+repeat task.wait() until Flag.ImageTransparency == 0
+
+local OriginalPositions = {}
+OriginalPositions[1] = LeftSideText.Position
+OriginalPositions[2] = RightSideText.Position
+
+LeftSideText:TweenPosition(UDim2.new(0, 0, 0.25, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+task.wait(1)
+RightSideText:TweenPosition(UDim2.new(0, 0, 0.25, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+
+task.wait(2)
+
+LeftSideText:TweenPosition(OriginalPositions[1], Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+task.wait(1)
+RightSideText:TweenPosition(OriginalPositions[2], Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+task.wait(1)
+
+while task.wait() do
+    if Flag.ImageTransparency == 1 then
+        break
+    end
+    Flag.ImageTransparency = Flag.ImageTransparency + 0.01
+end
+
+repeat task.wait() until Flag.ImageTransparency == 1
+
+getByName("Support me"):Destroy()
