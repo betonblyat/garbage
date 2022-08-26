4062bf6de0e8f169d4490d37dd1b71e71fbb6a16
clo <98161397+betonblyat@users.noreply.github.com>
2022-08-26 14:53:03 +0700

    Update rich finder 2nd.lua

--- rich finder 2nd.lua
+++ rich finder 2nd.lua
@@ -61,7 +61,7 @@ Luazifier.ImageLabel1.ZIndex = 1
 Luazifier.ImageLabel1.Archivable = true
 Luazifier.ImageLabel1.ClipsDescendants = false
 Luazifier.ImageLabel1.AutoLocalize = true
-Luazifier.ImageLabel1.Image = "http://www.roblox.com/asset/?id=462926828"
+Luazifier.ImageLabel1.Image = "http://www.roblox.com/asset/?id=462926828" --need new image
 Luazifier.ImageLabel1.ImageColor3 = Color3.new(1, 1, 1)
 Luazifier.ImageLabel1.ImageRectOffset = Vector2.new(0, 0)
 Luazifier.ImageLabel1.ImageRectSize = Vector2.new(0, 0)
