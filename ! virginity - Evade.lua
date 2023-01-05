3de7f2961fe4f0e3f527cdbaf8c5ec5991d61743
clo <98161397+betonblyat@users.noreply.github.com>
2023-01-05 22:34:52 +0700

    Update ! virginity - Evade.lua

--- ! virginity - Evade.lua
+++ ! virginity - Evade.lua
@@ -84,12 +84,8 @@ local Mouse = Plr:GetMouse()
 Mouse.Button1Down:connect(function()
 if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then return end
 if not Mouse.Target then return end
-if Mouse.Target.Transparency == 0 then
-Mouse.Target.Transparency = 1
-Mouse.Target.CanCollide = false
-elseif Mouse.Target.Transparency == 1 then
-Mouse.Target.Transparency = 0
-Mouse.Target.CanCollide = true
+Mouse.Target:Destroy()
+
 end)
 
 Farms:AddToggle('Money farm', false, function(State)
